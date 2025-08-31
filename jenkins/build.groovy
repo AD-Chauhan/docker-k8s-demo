3fcfcf7ea03ab67f426e7944a56a5cf6df78dc2d
AD-Chauhan <deep.epri@gmail.com>
2025-08-31 09:03:53 +0530

    updated groovy script

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -10,7 +10,7 @@ pipeline{
 	stages{
 	stage("Checkout from GitHub"){
 		steps{
-			git 'https://github.com/AD-Chauhan/docker-k8s-app.git'
+			git 'https://github.com/AD-Chauhan/docker-k8s-demo.git'
 		}
 	}
 	
